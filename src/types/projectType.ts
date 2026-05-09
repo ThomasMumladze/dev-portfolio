d098adf0ddb7e123042eccd88985c7cf28364971
ThomasMumladze <mumladzea4@gmail.com>
2026-05-09 23:31:21 +0400

    dev

--- src/types/projectType.ts
+++ src/types/projectType.ts
@@ -1,12 +1,11 @@
 type ProjectType = "back-end" | "front-end" | "full-stack" | "mobile" | "api";
-type Language = "Java" | "TypeScript" | "Python" | "C#" | "Kotlin";
 type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
 
 export interface Project {
     id: number;
     applicationName: string;
     type: ProjectType;
-    language: Language;
+    language: string;
     technologies: string[];
     methods: HttpMethod[];
     description: string;
