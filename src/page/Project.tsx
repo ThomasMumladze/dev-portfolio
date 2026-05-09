6d6982ce89935f826a19f542a857df33477604b9
ThomasMumladze <mumladzea4@gmail.com>
2026-05-10 02:40:12 +0400

    dev

--- src/page/Project.tsx
+++ src/page/Project.tsx
@@ -1,6 +1,7 @@
 import { IoSearchOutline } from "react-icons/io5";
 
 import _project from "../assets/data/project.json";
+import type { ProjectType } from "../types/projectType";
 
 import Card from "../components/Card";
 
@@ -8,6 +9,7 @@ const type = ["back-end", "front-end", "full-stack", "console app", "game"];
 const tech = ["java", "MsSql", "css", "scss", "React.js", "javascript", "TypeScript", ".Net Core", "C#", "Unity"];
 
 const Project = () => {
+    const projectData = _project as unknown as ProjectType[];
     return (
         <article className="project-page">
             <div className="page__filter">
@@ -25,7 +27,11 @@ const Project = () => {
                 </div>
             </div>
             <p>{_project.length} project founded</p>
-            <div className="product__list">{_project && _project.map((item) => <Card key={item.id} data={item} />)}</div>
+            <div className="product__list">
+                {projectData.map((item: ProjectType) => (
+                    <Card key={item.id} data={item} />
+                ))}{" "}
+            </div>
         </article>
     );
 };
