f49c615d323ff6d7fdde39f17fae9358f8702da9
ThomasMumladze <mumladzea4@gmail.com>
2026-05-09 04:59:40 +0400

    add header

---
+++ src/layout/Header.tsx
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-content">
+                <h1>thomas mumladze</h1>
+                <Navigation />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
