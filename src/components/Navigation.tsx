d37e92205a9a0747e403beb2af57e026bd8da8da
ThomasMumladze <mumladzea4@gmail.com>
2026-05-09 04:59:29 +0400

    add navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <span>#</span>home
+                </li>
+                <li>
+                    <span>#</span>project
+                </li>
+                <li>
+                    <span>#</span>about-me
+                </li>
+                <li>
+                    <span>#</span>contact
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
