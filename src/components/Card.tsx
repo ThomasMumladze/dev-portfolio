e58ca334534f33df74568f4593ac91b1b4682a59
ThomasMumladze <mumladzea4@gmail.com>
2026-05-10 17:10:12 +0400

    dev

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,6 +14,8 @@ interface Props {
 
 const Card = (props: Props) => {
     const { data } = props;
+    console.log(data.methods.length);
+
     return (
         <div className="card-wrapper">
             <div className="top-card">
