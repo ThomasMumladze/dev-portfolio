97e2179585a2d257a739d5be3fed30fadc8020e8
ThomasMumladze <mumladzea4@gmail.com>
2026-05-13 22:31:27 +0400

    fixed home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -9,25 +9,45 @@ import _projectData from "../assets/data/project.json";
 
 // ==========  type for project data ==========  //
 import type { ProjectType } from "../types/projectType";
+import H1 from "../components/H1";
 
 const Home = () => {
     const projectData = _projectData as unknown as ProjectType[];
+    console.log(projectData.find((x) => x.type == "front-end"));
 
     return (
         <article className="home-page">
-            <section id="project">
+            <section id="front-end">
                 <div className="section-header">
-                    <h1>
-                        <span>#</span>Projects
-                    </h1>
+                    <H1 title="front-end" />
                     <Link to="/project">view all</Link>
                 </div>
 
                 {/* ========== mapping list of project ==========  // */}
                 <div className="project-list">
-                    {projectData.slice(0, 4).map((item: ProjectType) => (
-                        <Card key={item.id} data={item} />
-                    ))}
+                    {projectData
+                        .filter((x: ProjectType) => x.type === "front-end")
+                        .slice(0, 4)
+                        .map((item: ProjectType) => (
+                            <Card key={item.id} data={item} />
+                        ))}
+                </div>
+            </section>
+
+            <section id="back-end">
+                <div className="section-header">
+                    <H1 title="back-end" />
+                    <Link to="/project">view all</Link>
+                </div>
+
+                {/* ========== mapping list of project ==========  // */}
+                <div className="project-list">
+                    {projectData
+                        .filter((x: ProjectType) => x.type === "back-end")
+                        .slice(0, 4)
+                        .map((item: ProjectType) => (
+                            <Card key={item.id} data={item} />
+                        ))}
                 </div>
             </section>
         </article>
