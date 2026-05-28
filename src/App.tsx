51e7a4a1374b9e90dbaffae93f208ed1927775c3
ThomasMumladze <mumladzea4@gmail.com>
2026-05-28 21:27:14 +0400

    app update

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,29 @@ import { Outlet, useLocation } from "react-router";
 
 // ========== component ========== //
 import Header from "./layout/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./layout/Footer";
+import ContextMenu from "./components/ContextMenu";
+import CustomConsole from "./components/CustomConsole";
 
 function App() {
     const location = useLocation();
-
+    const [showConsole, setShowConsole] = useState(false);
     useEffect(() => {
+        console.log("Hello From", location.pathname.slice(1, -1));
+
         window.scrollTo(0, 0);
     }, [location.pathname]);
+
     return (
         <>
+            <ContextMenu showConsole={showConsole} setShowConsole={setShowConsole} />
             {location.pathname === "/project-details" ? null : <Header />}
 
             <main>
                 <Outlet />
                 {/* <AdminProjects /> */}
+                <CustomConsole showConsole={showConsole} />
             </main>
 
             <Footer />
