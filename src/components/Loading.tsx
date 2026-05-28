bc3a00456c7c2f60c979d07113603cf17e6e6f55
ThomasMumladze <mumladzea4@gmail.com>
2026-05-28 19:35:59 +0400

    remove log

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,6 @@ interface Props {
 }
 const Loading = (props: Props) => {
     const { loadingStart } = props;
-    console.log(loadingStart, "child");
 
     return <div className={`loader`} style={{ animation: `l3 ${loadingStart}ms infinite` }}></div>;
 };
