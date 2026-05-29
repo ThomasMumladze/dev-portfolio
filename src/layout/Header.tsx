5775e858b46a281f19258688b78e06ed43e541c9
ThomasMumladze <mumladzea4@gmail.com>
2026-05-29 17:40:06 +0400

    update header nav when authenticated

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,51 @@
 // ========== component ========== //
+import { Link } from "react-router";
+
 import Navigation from "../components/Navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API = "https://localhost:7196";
+
+axios.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            await axios.post(`${API}/api/Auth/refresh`, {}, { withCredentials: true });
+
+            return axios(error.config);
+        }
+        return Promise.reject(error);
+    },
+);
 
 const Header = () => {
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get(`${API}/api/Auth/check`, { withCredentials: true })
+            .then(() => setIsAuth(true))
+            .catch(() => setIsAuth(false));
+    }, []);
+
     return (
         <header>
             <div className="header-content">
                 <h1>thomas mumladze</h1>
-                <Navigation />
+                <div>
+                    <Navigation />
+
+                    {isAuth ? (
+                        <Link className="login-page-link" to="/admin-dashboard">
+                            admin
+                        </Link>
+                    ) : (
+                        <Link className="login-page-link" to={"/log-in"}>
+                            login
+                        </Link>
+                    )}
+                </div>
             </div>
         </header>
     );
