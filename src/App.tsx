9b56ad295f08dd99d7e2c5705ddce2445349a62a
ThomasMumladze <mumladzea4@gmail.com>
2026-05-29 17:41:03 +0400

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import "./App.scss";
 
+// ========== React Hook ========== //
+import { useEffect, useState } from "react";
+
 // ========== react-router ========== //
 import { Outlet, useLocation } from "react-router";
 
 // ========== component ========== //
-import Header from "./layout/Header";
-import { useEffect, useState } from "react";
-import Footer from "./layout/Footer";
 import ContextMenu from "./components/ContextMenu";
 import CustomConsole from "./components/CustomConsole";
 
+// ========== Layouts ========== //
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
+// ========== Authorization ========== //
+import useAuthorization from "./hook/useAuthorization";
+import type { Authorization } from "./types/authorization";
+
 function App() {
+    const { ...authorization } = useAuthorization() as Authorization;
     const location = useLocation();
     const [showConsole, setShowConsole] = useState(false);
+
     useEffect(() => {
         console.log("Hello From", location.pathname.slice(1, -1));
 
@@ -25,8 +35,8 @@ function App() {
             {location.pathname === "/project-details" ? null : <Header />}
 
             <main>
-                <Outlet />
-                {/* <AdminProjects /> */}
+                <Outlet context={{ authorization }} />
+
                 <CustomConsole showConsole={showConsole} setShowConsole={setShowConsole} />
             </main>
 
