844b97fdedc7e160b16c524157d5996812b7567c
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-06-02 23:58:14 +0400

    add skills constant

---
+++ src/constants/Skills.ts
@@ -0,0 +1,28 @@
+export const FRONT_END_SKILLS = [
+    "javascript",
+    "typescript",
+    "react.js",
+    "scss",
+    "sass",
+    "styled-components",
+    "tailwindcss",
+    "react-router",
+    "axios",
+    "react-icons",
+    "react-bootstrap",
+];
+
+export const BACK_END_SKILLS = ["c sharp", "java", "java spring", "api", "mssql"];
+
+export const TOOL_SKILLS = [
+    "gitHub",
+    "azure",
+    "cmd",
+    "powerShell",
+    "figma",
+    "photoshop",
+    "postman",
+    "visual studio code",
+    "visual studio",
+    "jenkins",
+];
